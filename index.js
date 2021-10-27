f34ddfeebf1a09b2f5db9ce01aa1ae6731c464ba
tushergoon <tushergoondip@gmail.com>
2021-10-28 03:05:35 +0600

    here

--- index.js
+++ index.js
@@ -63,6 +63,9 @@ run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Running servre car')
+});
+app.get('/hello', (req, res) => {
+    res.send('hello update')
 })
 
 app.listen(port, () => {
